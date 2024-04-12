bc8272fdb27944fe44371219d86559214b9f5e87
Miray Karagöz <mmiraykaragoz@hotmail.com>
2024-04-13 00:52:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HavaDurumu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hava durumu uygulamasına hoşgeldiniz. Devam etmek için lütfen enter'a basınız.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Hava durumunu giriniz: ");
+
+            int havaDurumu = int.Parse(Console.ReadLine());
+
+            if (havaDurumu <= 10)
+            {
+                Console.WriteLine("Hava çok soğuk. Kıyafetinizin üstünüze kaban/palto alıp ardından atkınızı ve şapkanızı takabilirsiniz.");
+            }
+            else if (havaDurumu <= 20)
+            {
+                Console.WriteLine("Hava esiyor. Dış giyimde kaban/palto yerine daha ince ceketler tercih edebilirsiniz. Trençkot vb.");
+            }
+            else
+            {
+                Console.WriteLine("Hava sıcak. Havalar sıcakken en çok tercih edilen keten kumaşlardan kıyafetler tercih edebilirsiniz. Hem ince ve ferah hem de sıcak havalara birebir.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
